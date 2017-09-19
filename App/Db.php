184d4ee73c8122aa89306e1e329d25d7da42bcea
andrey <volosgoto@yahoo.com>
2017-09-19 09:35:43 +0300

    Db class ready

--- App/Db.php
+++ App/Db.php
@@ -6,7 +6,7 @@ use PDO;
 class Db {
 
     protected $host = 'localhost';
-    protected $db   = 'test';
+    protected $db   = 'onemoreframework';
     protected $user = 'root';
     protected $pass = '';
     protected $charset = 'utf8';
@@ -18,16 +18,27 @@ class Db {
     ];
 
     protected $dsn;
-    protected $dbh;
+    protected $dbh; // database handler
+
 
     public function __construct() {
         $this->dsn = "mysql:host=$this->host; dbname=$this->db; charset=$this->charset";
         $this->dbh = new \PDO($this->dsn, $this->user, $this->pass, $this->opt);
     }
+
     public function execute($sql){
         $sth = $this->dbh->prepare($sql);
         $res = $sth->execute();
-
         return $res;
+    }
+
+    public function query($sql){
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth->execute();
+            if (false !== $res){
+                return $sth->fetchAll();
+            } else {
+                return [];
+            }
     }
 }
--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 require __DIR__ . '/autoload.php';
 
 $db = new \App\Db();
-$res = $db->execute("CREATE TABLE foo (id SERIAL)");
-
-var_dump($res);
+//$res = $db->execute("CREATE TABLE foo (id SERIAL)");
+//var_dump($res);
+$data  = $db->query('SELECT * FROM foo');
+var_dump($data);
 
 ?>
